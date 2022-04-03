f9ebf6352f6ee676a91bf7832208d4492c2e4de5
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:38:17 +0500

    Add files via upload
    Task 10.9
    Creat a function to check the
    Square ,
    Cube
    Square root of any number

---
+++ 10.9.py
@@ -0,0 +1,19 @@
+print('=============Task 10.9===========')
+import math
+#square of function
+def sqr():
+      square = int(input('enter the number:\n'))
+      print("square of given number is\n",(square)**2)
+sqr()
+#cube function
+def cube():
+    cube = int(input('enter the number:\n'))
+    print("cube of given number is:\n",cube**3)
+cube()
+#square root of any num
+def sqrt():
+     sqrt = int(input('enter the number:\n'))
+     print("the sqrt of given number is:",(sqrt)*1/2)
+sqrt()
+
+
