c7c5ce42c77d328193b87006e00e6066c57e0a9b
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:37:31 +0500

    Add files via upload
    Task 10.6
    Write a Python function to calculate the factorial of a number (a non-negative integer).

---
+++ 10.6.py
@@ -0,0 +1,6 @@
+print("=======================Task10.6==================")
+import math
+def factorial():
+    user = int(input("Enter the number:\n"))
+    print("The factorial of:",user,"is",math.factorial(user))
+factorial()
