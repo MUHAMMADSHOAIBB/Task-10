2332b3db66564bd83ea5aa837e4994cc1ac5f056
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:36:25 +0500

    Add files via upload
    Task 10.2
     Create a function that helps to solve the quadratic equation.

---
+++ 10.2.py
@@ -0,0 +1,11 @@
+# we have to write a program to solve quadratic equation
+#formula of quadratic equation is: ax^2 +bx+c=0
+# we have to find the value of x s0  =  −b + √(b2 − 4ac)/2a ,and −b - √(b2 − 4ac)/2a
+def quad():
+    a = int(input('enter the value of a'))
+    b = int(input('enter the value of b'))
+    c = int(input('enter the value of c'))
+    x = ((-b+(b**2-4*a*c)*1/2)/2*a)
+    y = ((-b-(b**2-4*a*c)*1/2)/2*a)
+    print("the value of x is :",x,"\nThe value of y is:",y)
+quad()
