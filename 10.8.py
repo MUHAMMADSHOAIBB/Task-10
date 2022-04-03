1a9b5e8d2267a851238acf43f5ff39c2ff49d1a6
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:38:02 +0500

    Add files via upload
    Task 10.8
    Define a function that accepts radius and returns the area of a circle.

---
+++ 10.8.py
@@ -0,0 +1,6 @@
+print('=================Task10.8=================')
+import math
+def area():
+       r = int(float((input ("Input the radius of the circle : "))))
+       print ("The area of the circle with radius " + str(r) + " is: " + str(math.pi * r**2))
+area()
