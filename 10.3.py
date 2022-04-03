92b07632758f13496ae004d626489055b8d45e62
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:36:42 +0500

    Add files via upload
    Task 10.3
    Creat a function that helps to solve the linear equation

---
+++ 10.3.py
@@ -0,0 +1,7 @@
+#the formula of linear equation is ax = b
+#if we have to find the value of x then we can solve this equation like x -b/a
+def linearequ():
+    a = int(input('enter the value of a\n'))
+    b = int(input('enter the value of b\n'))
+    print("the value of x is:",-b/a)
+linearequ()
