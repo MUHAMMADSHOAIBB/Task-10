a70a209e9942a7abede7f8ab1dd87ef5d6962e6e
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 13:38:49 +0500

    Add files via upload
    Task 10.11
    Create a GCI calculator

---
+++ 10.11.py
@@ -0,0 +1,6 @@
+def gci():
+    print("how to calculate GCI in real estate")
+    sp =  int(input("Enter the sale price of property:\n"))
+    cr = int(input("Enter the commission rate:\n "))
+    print("The GCI =",sp*(cr)/100)
+gci()
